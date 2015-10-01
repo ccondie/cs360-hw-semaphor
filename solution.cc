d0f93fa67130a1301e79aca7a41a35b65f7289d4
Clayton Condie <ccondie@new-host.home>
2015-09-30 22:52:50 -0600

    completed the semaphor project

--- solution.cc
+++ solution.cc
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <semaphore.h>
 
 #include <time.h>
 
@@ -12,8 +13,10 @@ using namespace std;
 struct shared
 {
 	int number;
-	pthread_mutex_t mutex;
-	pthread_cond_t cond;
+	// pthread_mutex_t mutex;
+	// pthread_cond_t cond;
+	sem_t sem;
+	sem_t cond;
 	bool written;
 
 };
@@ -31,8 +34,11 @@ main(int argc, char **argv)
 	struct shared share;
 
 	//initialize mutex
-	pthread_mutex_init(&share.mutex, NULL);
-	pthread_cond_init(&share.cond, NULL);
+	// pthread_mutex_init(&share.mutex, NULL);
+	// pthread_cond_init(&share.cond, NULL);
+	sem_init(&share.sem, 0 ,1);
+	sem_init(&share.cond, 0 ,0);
+
 	share.written = false;
 	share.number = 0;
 	srand(time(NULL));
@@ -58,19 +64,19 @@ write(void *vptr)
 
 
 	
-	pthread_mutex_lock(&s->mutex);
+	// pthread_mutex_lock(&s->mutex);
+	sem_wait(&s->sem);
 
 	int random = rand() % 100 + 1;
 	s->number = random;
 	s->written = true;
 	cout << "Storing: " << random  << endl;
 
-	pthread_cond_signal(&s->cond);
-	pthread_mutex_unlock(&s->mutex);
+	// pthread_cond_signal(&s->cond);
+	// pthread_mutex_unlock(&s->mutex);
 
-
-
-	usleep(1);
+	sem_post(&s->sem);
+	sem_post(&s->cond);
 }
 
 void *
@@ -79,18 +85,18 @@ read(void *vptr)
 	struct shared* s = (struct shared*) vptr;
 
 
+	sem_wait(&s->cond);
+	sem_wait(&s->sem);
+	// pthread_mutex_lock(&s->mutex);
 
-
-	pthread_mutex_lock(&s->mutex);
-
-	while(s->written == false)
-	{
-		pthread_cond_wait(&s->cond, &s->mutex);
-	}
+	// while(s->written == false)
+	// {
+		// pthread_cond_wait(&s->cond, &s->mutex);
+	// }
 	
 	cout << "Reading: " << s->number  << endl;
 
-	pthread_mutex_unlock(&s->mutex);
-	usleep(1);
+	sem_post(&s->sem);
 
+	// pthread_mutex_unlock(&s->mutex);
 }
